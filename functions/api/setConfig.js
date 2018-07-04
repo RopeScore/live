9d712d9747a4fbdb8a05a44c3c1fc3bedd69c4f5
Svante Bengtson <svante.b37@gmail.com>
2018-07-04 10:53:32 +0200

    add group field to config API

--- functions/api/setConfig.js
+++ functions/api/setConfig.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin')
 
+const del = admin.firestore.FieldValue.delete
+
 /**
  * @api {post} /:fed/:cat Set Category config
  * @apiName setConfig
  * @apiGroup Categories
  * @apiPermission federation
- * @apiVersion 1.0.0
+ * @apiVersion 1.3.0
  *
  * @apiHeader {String} Authorization Bearer with api key (<code>Bearer &lt;apikey&gt;</code>)
  *
@@ -13,6 +15,7 @@ const admin = require('firebase-admin')
  * @apiParam {String} cat id of the category
  *
  * @apiParam {String} name The new name of the Category
+ * @apiParam {String} [group] The category group the category is in
  * @apiParam {Object[]} events new Array of enabled events
  * @apiParam {String} events.abbr The "standard" abbreviation of the event
  * @apiParam {String} events.name The preferred name of the event, if not avilable the abbr will be used
@@ -41,12 +44,13 @@ module.exports = (req, res, next) => {
     }
   })
   let config = {
-    name: req.body.name,
+    name: req.body.name || 'Unnamed',
+    group: req.body.group || del(),
     events: events
   }
   admin.firestore().collection('live').doc('federations').collection(req.params.fed).doc('categories').set({})
   admin.firestore().collection('live').doc('federations').collection(req.params.fed).doc('categories')
-    .collection(req.params.cat).doc('config').set(config, {mergeFields: ['events', 'name']})
+    .collection(req.params.cat).doc('config').set(config, {mergeFields: ['events', 'name', 'group']})
     .then(ref => {
       res.json({message: 'Category Config Updated successfully'})
     })
@@ -55,3 +59,28 @@ module.exports = (req, res, next) => {
       next({statusCode: 500, error: 'Could not update category config'})
     })
 }
+
+/**
+ * @api {post} /:fed/:cat Set Category config
+ * @apiName setConfig
+ * @apiGroup Categories
+ * @apiPermission federation
+ * @apiVersion 1.0.0
+ *
+ * @apiHeader {String} Authorization Bearer with api key (<code>Bearer &lt;apikey&gt;</code>)
+ *
+ * @apiParam {String} fed federation
+ * @apiParam {String} cat id of the category
+ *
+ * @apiParam {String} name The new name of the Category
+ * @apiParam {Object[]} events new Array of enabled events
+ * @apiParam {String} events.abbr The "standard" abbreviation of the event
+ * @apiParam {String} events.name The preferred name of the event, if not avilable the abbr will be used
+ * @apiParam {Boolean} events.speed if the event is a speed event or not
+ * @apiParam {Object} events.cols columns to show
+ * @apiParam {String[]} events.cols.overall columns to show in the overall table
+ * @apiParam {String[]} events.cols.event columns to show in the event's table
+ *
+ * @apiSuccess {String} message success message
+ * @apiError   {String} message error message
+ */
