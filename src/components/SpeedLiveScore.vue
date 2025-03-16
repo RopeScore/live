461354add2a6307b942c16213ed5a2487af1eb86
Svante Bengtson <svante@swantzter.se>
2025-03-16 20:34:04 +0100

    npm up

--- src/components/SpeedLiveScore.vue
+++ src/components/SpeedLiveScore.vue
@@ -47,7 +47,7 @@
       {{ tally?.step ?? 0 }}
     </div>
 
-    <div class="debug absolute top-2 right-2 text-gray-500">
+    <div class="debug absolute top-2 right-auto left-auto text-gray-500">
       <div v-if="info.deviceId">
         {{ info.deviceId }}
       </div>
